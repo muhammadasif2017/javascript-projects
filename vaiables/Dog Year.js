b1140d95bb3c8efaa65f25e036e97298d5986895
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2021-08-20 18:41:05 +0500

    Written down the code of the Dog Year Project

--- vaiables/Dog Year.js
+++ vaiables/Dog Year.js
@@ -4,3 +4,25 @@ Here’s how you convert your age from “human years” to “dog years”:
 The first two years of a dog’s life count as 10.5 dog years each.
 Each year following equates to 4 dog years.
 */
+// age of the perosn 
+const myAge = 30;
+// variable to stoer the early Years value
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// later year stores the value minus the ealyYears intialization value 
+let laterYears = myAge - 2;
+// Multiplication assignement
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+// added the earlyYear age with later year age to 
+// calculate the age in dog year
+const myAgeInDogYears = earlyYears + laterYears;
+
+// lowercase the name 
+const myName = "Muhammad Asif".toLowerCase();
+
+// printing the message on console
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
